86eb731c148e559cf3516f86752bd50ec6f324f1
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-10-02 20:20:13 -0300

    feat: created dbContext and set tables

---
+++ HeroApi/Data/HeroContext.cs
@@ -0,0 +1,20 @@
+﻿using HeroApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeroApi.Data
+{
+    public class HeroContext : DbContext
+    {
+        public HeroContext(DbContextOptions<HeroContext> options) : base(options) { }
+
+        public DbSet<Hero> Heroes { get; set; }
+        public DbSet<Superpower> Superpowers { get; set; }
+        public DbSet<HeroSuperpower> HeroSuperpowers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroSuperpower>()
+                .HasKey(hs => new { hs.HeroId, hs.SuperpowerId });
+        }
+    }
+}
