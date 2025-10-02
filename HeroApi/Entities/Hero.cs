4d757d5690df8fb37441599756424b7a7651ebab
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-10-02 20:15:49 -0300

    feat: created entities Hero, Superpower and HeroSuperpower

---
+++ HeroApi/Entities/Hero.cs
@@ -0,0 +1,14 @@
+﻿namespace HeroApi.Entities
+{
+    public class Hero
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string HeroName { get; set; } = string.Empty;
+        public DateTime BirthDate { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+
+        public ICollection<HeroSuperpower> HeroSuperpowers { get; set; } = new List<HeroSuperpower>();
+    }
+}
---
+++ HeroApi/Entities/Superpower.cs
@@ -0,0 +1,9 @@
+﻿namespace HeroApi.Entities
+{
+    public class Superpower
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<HeroSuperpower> HeroSuperpowers { get; set; } = new List<HeroSuperpower>();
+    }
+}
