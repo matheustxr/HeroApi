e83bf7e746195904138f02f2fad4adaa9a087dd7
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-10-03 17:09:06 -0300

    feat: go to production

--- HeroApi/Program.cs
+++ HeroApi/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://localhost:5173")
+                          policy.WithOrigins("https://hero-frontend-x9gb.onrender.com/")
                                 .AllowAnyHeader()
                                 .AllowAnyMethod();
                       });
