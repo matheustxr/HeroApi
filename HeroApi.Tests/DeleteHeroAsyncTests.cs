cfb2675564b5eb467fbf4c7037b46671614381f3
Matheus Teixeira <matheustxr.dev@gmail.com>
2025-10-03 16:23:01 -0300

    feat: created tests for update and delete hero

---
+++ HeroApi.Tests/DeleteHeroAsyncTests.cs
@@ -0,0 +1,45 @@
+﻿using FluentAssertions;
+using HeroApi.Data;
+using HeroApi.Entities;
+using HeroApi.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace HeroApi.Tests;
+
+public class DeleteHeroAsyncTests
+{
+    private async Task<HeroContext> GetDbContext()
+    {
+        var options = new DbContextOptionsBuilder<HeroContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+        var context = new HeroContext(options);
+        await context.Database.EnsureCreatedAsync();
+        return context;
+    }
+
+    [Fact]
+    public async Task DeleteHeroAsync_WithValidId_ShouldReturnTrue()
+    {
+        var context = await GetDbContext();
+        context.Heroes.Add(new Hero { Id = 1, HeroName = "Superman" });
+        await context.SaveChangesAsync();
+        var heroService = new HeroService(context);
+
+        var result = await heroService.DeleteHeroAsync(1);
+
+        result.Should().BeTrue();
+        (await context.Heroes.CountAsync()).Should().Be(0);
+    }
+
+    [Fact]
+    public async Task DeleteHeroAsync_WithInvalidId_ShouldReturnFalse()
+    {
+        var context = await GetDbContext();
+        var heroService = new HeroService(context);
+
+        var result = await heroService.DeleteHeroAsync(99);
+
+        result.Should().BeFalse();
+    }
+}
--- HeroApi.Tests/UpdateHeroAsyncTests.cs
+++ HeroApi.Tests/UpdateHeroAsyncTests.cs
@@ -1,12 +1,13 @@
 ﻿using FluentAssertions;
 using HeroApi.Data;
+using HeroApi.DTOs;
 using HeroApi.Entities;
 using HeroApi.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace HeroApi.Tests;
 
-public class DeleteHeroAsyncTests
+public class UpdateHeroAsyncTests
 {
     private async Task<HeroContext> GetDbContext()
     {
@@ -14,32 +15,55 @@ public class DeleteHeroAsyncTests
             .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
             .Options;
         var context = new HeroContext(options);
-        await context.Database.EnsureCreatedAsync();
+        context.Database.EnsureCreated();
+
+        if (!await context.Superpowers.AnyAsync())
+        {
+            context.Superpowers.Add(new Superpower { Id = 1, SuperPower = "Voo" });
+            await context.SaveChangesAsync();
+        }
+
         return context;
     }
 
     [Fact]
-    public async Task DeleteHeroAsync_WithValidId_ShouldReturnTrue()
+    public async Task UpdateHeroAsync_WithValidData_ShouldUpdateAndReturnHero()
     {
         var context = await GetDbContext();
-        context.Heroes.Add(new Hero { Id = 1, HeroName = "Superman" });
+        context.Heroes.Add(new Hero { Id = 1, HeroName = "Superman", Name = "Clark Kent" });
         await context.SaveChangesAsync();
         var heroService = new HeroService(context);
+        var request = new RequestHeroJson { Name = "Kal-El", HeroName = "Superman", SuperpowerIds = new List<int> { 1 } };
 
-        var result = await heroService.DeleteHeroAsync(1);
+        var result = await heroService.UpdateHeroAsync(1, request);
 
-        result.Should().BeTrue();
-        (await context.Heroes.CountAsync()).Should().Be(0);
+        result.Should().NotBeNull();
+        result?.Name.Should().Be("Kal-El");
     }
 
     [Fact]
-    public async Task DeleteHeroAsync_WithInvalidId_ShouldReturnFalse()
+    public async Task UpdateHeroAsync_WithInvalidId_ShouldReturnNull()
     {
         var context = await GetDbContext();
         var heroService = new HeroService(context);
+        var request = new RequestHeroJson { SuperpowerIds = new List<int> { 1 } };
 
-        var result = await heroService.DeleteHeroAsync(99);
+        var result = await heroService.UpdateHeroAsync(99, request);
 
-        result.Should().BeFalse();
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task UpdateHeroAsync_WithDuplicateHeroName_ShouldThrowInvalidOperationException()
+    {
+        var context = await GetDbContext();
+        context.Heroes.AddRange(new Hero { Id = 1, HeroName = "Superman" }, new Hero { Id = 2, HeroName = "Batman" });
+        await context.SaveChangesAsync();
+        var heroService = new HeroService(context);
+        var request = new RequestHeroJson { HeroName = "Batman", SuperpowerIds = new List<int> { 1 } };
+
+        Func<Task> act = async () => await heroService.UpdateHeroAsync(1, request);
+
+        await act.Should().ThrowAsync<InvalidOperationException>();
     }
 }
